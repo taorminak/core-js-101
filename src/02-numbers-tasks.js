4c2da295fbe805f5cf14a791f49bb4034cf80f2c
taorminak <ostanina.anna@yahoo.com>
2023-05-31 17:56:23 +0200

    fix: fix get average function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -53,7 +53,8 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  return (value1 + value2) / 2;
+  const average = (value1 / 2) + (value2 / 2);
+  return average;
 }
 
 /**
