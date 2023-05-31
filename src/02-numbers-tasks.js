36fa06599954b51d33ca6b5ee56663bc3bdc55ff
taorminak <ostanina.anna@yahoo.com>
2023-05-31 23:30:30 +0200

    feat: fix functions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -149,7 +149,7 @@ function getLastDigit(value) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(value) {
-  const num = parseInt(value, 10);
+  const num = parseFloat(value);
   return num;
 }
 
